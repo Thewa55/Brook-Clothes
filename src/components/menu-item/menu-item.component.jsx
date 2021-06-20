cdfc4da83342ebd2e774836872ab52335050eb49
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-20 19:26:13 -0400

    Added css to the menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,8 @@ import './menu-item.style.scss'
 //destructuring the title from the props
 const MenuItem = ({title, url, size})=> {
     return (
-        <div style={{backgroundImage: `url(${url})`}} className={`menu-item  ${size}`}>
+        <div className={`menu-item  ${size}`}>
+            <div style={{backgroundImage: `url(${url})`}} className='background-image'/>
             <div className='content'>
                 <h1 className ='title'>{title}</h1>
                 <span className='subtitle'>SHOP NOW</span>
