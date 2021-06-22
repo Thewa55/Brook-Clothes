ae2b02a0148a39e3956bb444cd8292e9a4165b91
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-21 22:40:34 -0400

    Added react router dom and routing

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,6 +5,7 @@ import Directory from '../../components/directory/directory.component';
 const Homepage = () => {
     return(<div className='homepage'>
         <div className='directory'>
+            {/* This will dynamically link the button to the hats page <button onClick= {() => props.history.push('/hats')}>Hats</button> */}
             <Directory />
         </div>
     </div>)
