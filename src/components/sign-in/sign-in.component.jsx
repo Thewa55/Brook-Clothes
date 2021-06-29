9e8ded03f918da376bd1768fdc355d56f55c63f9
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-28 22:39:14 -0400

    Added a button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 import './sign-in.style.scss'
 
 const SignIn = () => {
@@ -35,7 +36,7 @@ const SignIn = () => {
                     handleChange= {handleChange}
                     label='Password'
                     required />     
-                <input type='submit' value='Submit Form'/>
+                <CustomButton type='submit'> Sign In </CustomButton>
             </form>
         </div>
     )
