5eb8a1b32e839e78c8f72b56d98f63588a7867b1
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-29 21:53:25 -0400

    Added authentication to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.util'
 
 function App() {
+
+  const [user, setUser ] = useState()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user)
+    })
+
+    console.log(user)
+  })
+
   return (
     <div className="App">
       <Header />
