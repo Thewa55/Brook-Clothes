8127aae4e01c43fc7619c1954e0f14f0a19894f8
Kenny Lam <kinwai.lam730@gmail.com>
2021-07-10 00:03:14 -0400

    Added a new action type for cart reducer to add items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.action';
 
 import './collection-item.style.scss'
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = (item) => {
+    
+    const dispatch = useDispatch();
+    const { name, price, imageUrl } = item.item;
+    
+    return(
     <div className='collection-item'>
         <div className='image' style= {{backgroundImage: `url(${imageUrl})`}} />
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <CustomButton inverted>Add to cart</CustomButton>
+        <CustomButton onClick={() => dispatch(addItem(item))} inverted>Add to cart</CustomButton>
     </div>
-)
+    )
+}
 
 export default CollectionItem;
