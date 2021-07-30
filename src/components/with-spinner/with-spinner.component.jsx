acb3cfa642392bb31199f928e3b0bacbb0244e55
Kenny Lam <kinwai.lam730@gmail.com>
2021-07-29 23:49:40 -0400

    Added with-spinner component

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.style';
+
+const withSpinner = WrappedComponent => { 
+    const Spinner = ({isLoading, ...otherProps}) => {
+    return isLoading ? (
+        <SpinnerOverlay>
+            <SpinnerContainer>
+
+            </SpinnerContainer>
+        </SpinnerOverlay>
+        ) : (
+            <WrappedComponent {...otherProps} />
+        )
+    }
+    return Spinner;
+};
+
+export default withSpinner
