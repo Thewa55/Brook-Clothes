0596637f2d09a3dc6c1863dabdc5fe2d8db01b79
Kenny Lam <kinwai.lam730@gmail.com>
2021-08-06 23:05:49 -0400

    Imported saga middleware and added saga to middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-import thunk  from "redux-thunk";
-
+import createSagaMiddleware from "@redux-saga/core";
 import rootReducer from "./root-reducer";
 
-const middlewares = [thunk];
+//old thunk code
+import thunk  from "redux-thunk";
+// const middlewares = [thunk];
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware, thunk]
+
 
 
 //Only have logger active only if it's development environment 
@@ -15,6 +21,8 @@ if(process.env.NODE_ENV === 'development'){
 
 //we are spreading the middleware so if we add more middleware we can add them into the array
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+//sagaMiddleware.run()
 
 const persistor = persistStore(store)
 
