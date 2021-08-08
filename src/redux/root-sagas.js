2530b5cdcde05ef1a8cc8fbb45d637ee60741b74
Kenny Lam <kinwai.lam730@gmail.com>
2021-08-08 11:13:41 -0400

    Resolved errors from previous commit

--- src/redux/root-sagas.js
+++ src/redux/root-sagas.js
@@ -5,7 +5,7 @@ export default function* rootSaga(){
 
     //all takes an array of Sagas, if we wanted to render ie 3 sagas all at once we put it into all effect. If all 3 sagas are on separate lines, we would hvae to wait for each Saga to finish 
     yield all([
-        //fetchCollectionsStart() along without call works as well, but best practice to use call
-        call(fetchCollectionsStart())
+        //fetchCollectionsStart() without call works as well, but best practice to use call
+        call(fetchCollectionsStart)
     ])
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import createSagaMiddleware from "@redux-saga/core";
 import rootReducer from "./root-reducer";
-import { rootSaga } from './root-saga'
+import rootSaga from './root-sagas'
 
 //Moved this into root reducer
 //import { fetchCollectionsStart } from "./shop/shop.sagas";
@@ -15,8 +15,6 @@ import { rootSaga } from './root-saga'
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware]
-
-
 
 //Only have logger active only if it's development environment 
 if(process.env.NODE_ENV === 'development'){
