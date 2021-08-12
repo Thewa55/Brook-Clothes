49788b6a4ee96d919ffc380ca3b78684be5969dc
Kenny Lam <kinwai.lam730@gmail.com>
2021-08-11 20:12:33 -0400

    Refactored Sagas code

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,29 +1,50 @@
 import { takeLatest, put, all, call } from "@redux-saga/core/effects";
 import UserActionTypes from "./user.types";
 import { auth, googleProvider, createUserProfileDocument } from "../../firebase/firebase.utils";
-import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from "./user.action";
+//import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from "./user.action";
+import { signInSuccess, signInFailure } from "./user.action";
+
+export function* getSnapshotFromUserAuth(userAuth){
+    try{
+    const userRef = yield call (createUserProfileDocument, userAuth)
+
+    const userSnapShot = yield userRef.get()
+
+    //the put(googleSignInSuccess) is similar to how we originally had it in the app.js file where we dispatched the user information into redux
+    // dispatch(setCurrentUser(
+    //     {id:snapShot.id,
+    //     ...snapShot.data()}
+    //   ))
+    yield put(signInSuccess({id: userSnapShot.id, ...userSnapShot}));
+
+    } catch (error){
+        yield put(signInFailure(error));
+    }
+}
+
 
 export function* onSignInWithGoogle(){
     try{
         //the reason we don't call signInWithGoogle method from the firebase utils is because we want to access the return value
         const { user } = yield auth.signInWithPopup(googleProvider);
+        yield getSnapshotFromUserAuth(user)
         
+        //refactored this part of the try catch into getSnapshotFromuserAuth function
         //we are passing the user into createUserProfileDocument and returning the user reference from firestore
-        const userRef = yield call (createUserProfileDocument, user)
-
-        const userSnapShot = yield userRef.get()
+        //const userRef = yield call (createUserProfileDocument, user)
+        //const userSnapShot = yield userRef.get()
 
         //the put(googleSignInSuccess) is similar to how we originally had it in the app.js file where we dispatched the user information into redux
         // dispatch(setCurrentUser(
         //     {id:snapShot.id,
         //     ...snapShot.data()}
         //   ))
-        yield put(googleSignInSuccess({id: userSnapShot.id, ...user}));
+        //yield put(signInSuccess({id: userSnapShot.id, ...user}));
 
 
     } catch (error){
 
-        yield put(googleSignInFailure(error));
+        yield put(signInFailure(error));
     }
 }
 
@@ -34,12 +55,15 @@ export function* onGoogleSignInStart(){
 export function* onSignInWithEmail({payload: { email, password}}){
     try {
         const { user } = yield auth.signInWithEmailAndPassword(email, password);
-        const userRef = yield call (createUserProfileDocument, user)
-        const userSnapShot = yield userRef.get()
-        yield put(emailSignInSuccess({id: userSnapShot.id, ...user}));
+        yield getSnapshotFromUserAuth(user)
+
+        //refactored this part of the try catch into getSnapshotFromuserAuth function
+        // const userRef = yield call (createUserProfileDocument, user)
+        // const userSnapShot = yield userRef.get()
+        // yield put(signInSuccess({id: userSnapShot.id, ...user}));
 
     } catch (error){
-        yield put(emailSignInFailure(error))
+        yield put(signInFailure(error))
     }
 }
 
